16967c6caea85b635bef669c48a33d494d71ae80
Michael Nguyen <michaelnguyen974@gmail.com>
2018-12-13 16:19:20 +0000

    refactor code using test helpers

--- spec/features/hitpoints_spec.rb
+++ spec/features/hitpoints_spec.rb
@@ -1,13 +1,12 @@
 require 'capybara/rspec'
 require './app.rb'
+require_relative 'web_helpers'
 Capybara.app = Battle
 
 feature "Hitpoints" do 
   scenario "see opponent's hitpoints" do 
   visit("/")
-  fill_in :player_1, with: "Jin"
-  fill_in :player_2, with: "Kazuya"
-  click_button 'Submit'
+  sign_in_and_play
   expect(page).to have_content "Hitpoints: 100"
   end 
 end 
--- spec/features/names_feature_spec.rb
+++ spec/features/names_feature_spec.rb
@@ -1,20 +1,11 @@
 require 'capybara/rspec'
 require './app.rb'
+require_relative 'web_helpers'
 Capybara.app = Battle
-
-# feature "show testing infrastructure working!!!" do 
-#   scenario "testing infrastructure working text appears" do 
-#     visit("/")
-#     expect(page).to have_content("Testing infrastructure working!")
-#   end 
-# end 
 
 feature "Enter names for TEKKEN" do 
   scenario "tests for filling in forms with names" do
-  visit("/")
-  fill_in :player_1, with: "Jin"
-  fill_in :player_2, with: "Kazuya"
-  click_button 'Submit'
+  sign_in_and_play
   expect(page).to have_content 'Jin vs Kazuya'
   end 
 end 
